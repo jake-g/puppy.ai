2c8a2d95bce7c965031c44616ef93f79a4528f69
jake-g <omonoid@uw.edu>
2017-09-20 21:07:30 -0700

    fixed typo that made the app not work

--- PAICNNRunner.mm
+++ PAICNNRunner.mm
@@ -176,7 +176,7 @@ int voteCount = 0; // counts frames where prediction is not necessary
         const float decayValue = 0.6f;  // low=fast decay
         const float updateValue = 0.4f; // low=slow increase
         const float minimumThreshold = 0.01f;
-        const float minPredictionValue = 0.5f;
+        const float minPredictionValue = 0.05f;
         
         NSMutableDictionary *decayedPredictionValues =
         [[NSMutableDictionary alloc] init];
